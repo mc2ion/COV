c0bbd4c3ff11d239ed0eb81415d7b869c931390a
Yomar Medina <ailema39@gmail.com>
2015-06-20 20:59:35 -0500

    Twitter timeline y slider

--- index.php
+++ index.php
@@ -3,7 +3,29 @@
   <head>
     <meta charset="utf-8">
     <title>Cómito Olímpico</title>
+    <link rel="stylesheet" href="css/slider.css"/>
     <link rel="stylesheet" href="style.css"/>
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/jssor.slider.mini.js"></script>
+    <script>
+        jQuery(document).ready(function ($) {
+            var options = { 
+                $AutoPlay: true,
+                $BulletNavigatorOptions: {
+                    $Class: $JssorBulletNavigator$,
+                    $ChanceToShow: 2,
+                    $SpacingX: 10,
+                    $SpacingY: 10,
+                    $AutoCenter: 1
+                },
+                $ArrowNavigatorOptions: {
+                    $Class: $JssorArrowNavigator$,
+                    $ChanceToShow: 2,
+                    $AutoCenter: 2                } 
+            };
+            var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+        });
+    </script>
   </head>
   <body>
     <div class="header">
@@ -48,8 +70,34 @@
     <div class="body">
         <div class="container">
             <div class="row">
-                <div class="noticias-slider"></div>
-                <div class="twitter-box"></div>
+                <div id="slider1_container" class="noticias-slider" style="position: relative; top: 0px; left: 0px; width: 645px; height: 389px;">
+                    <!-- Slides Container -->
+                    <div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width: 645px; height: 389px;">
+                        <div><img u="image" src="image1.jpg" /></div>
+                        <div><img u="image" src="image2.jpg" /></div>
+                        <div><img u="image" src="image3.jpg" /></div>
+                        <div><img u="image" src="image4.jpg" /></div>
+                        <div><img u="image" src="image5.jpg" /></div>
+                        <div><img u="image" src="image6.jpg" /></div>
+                        <div><img u="image" src="image7.jpg" /></div>
+                        <div><img u="image" src="image8.jpg" /></div>
+                    </div>
+                    <!-- Arrow Left -->
+                    <span u="arrowleft" class="jssora10l" style="top: 123px; left: 8px;">
+                    </span>
+                    <!-- Arrow Right -->
+                    <span u="arrowright" class="jssora10r" style="top: 123px; right: 8px;">
+                    </span>
+                    <!-- bullet navigator container -->
+                    <div u="navigator" class="jssorb01" style="bottom: 16px; right: 10px;">
+                        <!-- bullet navigator item prototype -->
+                        <div u="prototype"></div>
+                    </div>
+                </div>
+                <div class="twitter-box">
+                    <a class="twitter-timeline" href="https://twitter.com/PuntoOlimpico" data-widget-id="612438112430653445">Tweets por el @PuntoOlimpico.</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                </div>
                 <div class="actions">
                      <div class="posiciones sq"><a href=""></a></div>
                      <div class="olimpismo sq"><a href=""></a></div>
