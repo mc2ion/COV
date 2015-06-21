8adca3b12f291fb12b2773ec3f297e78016b2766
Marion <mc2.ions@gmail.com>
2015-06-20 21:31:25 -0500

    Estructura HTML - FOOTER

--- index.php
+++ index.php
@@ -6,172 +6,204 @@
     <link rel="stylesheet" href="style.css"/>
   </head>
   <body>
-    <div class="header">
-        <div class="container">
-            <div class="top-bar">
-                <ul class="social">
-                        <li class="twitter"><a href=""></a></li>
-                        <li class="facebook"><a href=""></a></li>     
-                        <li class="instagram"><a href=""></a></li>        
-                        <li class="youtube"><a href=""></a></li>        
-                        <li class="linkedin"><a href=""></a></li> 
-                </ul>
-                <ul class="share">
-                        <li class="share-icon"><a href=""></a></li>
-                        <li class="email-icon"><a href=""></a></li>     
-                        <li class="chat-icon"><a href=""></a></li>   
-                </ul>
-                <div class="search">
-                    <input type="text"/>
-                    <input id="search" value="search"/>
-                </div>
-            </div>
-            <nav>
-                <ul class="menu">
-                    <li><a href="">COV</a></li>
-                    <li><a href="">JJOO</a></li>
-                    <li><a href="">Ciclo Olímpico</a></li>
-                    <li><a href="">Disciplinas</a></li>
-                    <li><a href="">Atletas</a></li>
-                    <li><a href="">Federaciones</a></li>
-                    <li><a href="">Fotos</a></li>
-                    <li><a href="">Videos</a></li>
-                    <li><a href="">Noticias</a></li>
-                </ul>
-            </nav>
-            <div class="logo">
-                <img src="./img/logo.png"/>
-            </div>
-        </div> 
-        <div class="clear"></div>
-    </div>   
-    <div class="body">
-        <div class="container">
-            <div class="row">
-                <div class="noticias-slider"></div>
-                <div class="twitter-box"></div>
-                <div class="actions">
-                     <div class="posiciones sq"><a href=""></a></div>
-                     <div class="olimpismo sq"><a href=""></a></div>
-                     <div class="oficiales sq"><a href=""></a></div>
-                     <div class="boletin"><a href=""></a></div>
-                     <div class="media"><a href=""></a></div>
-                     <div class="calendario"><a href=""></a></div>
-                     <div class="toronto"><a href=""></a></div>
-                     <div class="contacto"><a href=""></a></div>
-                     <div class="ubicanos"><a href=""></a></div>
-                </div>
-                <div class="clear"></div>
-            </div>
-             <div class="row">
-                   <div class="noticias">
-                        <h1>Noticias</h1>
-                        <p>Blah blah algun texto irá aquí sobre las noticias más importantes. bli bli
-                        blu blu. Blah blah algun texto irá aquí sobre las noticias.</p>
-                        <p class="plus"><img src='./img/plus.png'/></p>
-                 </div>
-                 <div class="atletas">
-                    <div class="toronto_atletas"><a href=""></a></div>
-                     <div class="punto_olimpico"><a href=""></a></div>
-                 </div>
-                 <div class="jug">
-                    <img src="./img/jugador.png" class="jugador"/>
-                 </div> 
-             <div class="clear"></div>
-             </div>
-             <div class="row">
-                 <div class="banner">Publicidad</div>
-             </div>
-             <div class="row">
-                 <div class="videos"></div>
-                 <div class="galeria"></div>
-                 <div class="clear"></div>
-             </div>
-             <div class="row">
-                 <div class="redes">
-                    <span>Redes Sociales</span>
-                    <ul class="social-bottom">
-                        <li class="twitter-b"><a href=""></a></li>
-                        <li class="facebook-b"><a href=""></a></li>
-                        <li class="instagram-b"><a href=""></a></li>
-                        <li class="youtube-b"><a href=""></a></li>
-                        <li class="linkedin-b"><a href=""></a></li>
+    <div class="wrapper">
+        <div class="header">
+            <div class="container">
+                <div class="top-bar">
+                    <ul class="social">
+                            <li class="twitter"><a href=""></a></li>
+                            <li class="facebook"><a href=""></a></li>     
+                            <li class="instagram"><a href=""></a></li>        
+                            <li class="youtube"><a href=""></a></li>        
+                            <li class="linkedin"><a href=""></a></li> 
                     </ul>
-                 </div>
+                    <ul class="share">
+                            <li class="share-icon"><a href=""></a></li>
+                            <li class="email-icon"><a href=""></a></li>     
+                            <li class="chat-icon"><a href=""></a></li>   
+                    </ul>
+                    <div class="search">
+                        <input type="text"/>
+                        <input id="search" value="search"/>
+                    </div>
+                </div>
+                <nav>
+                    <ul class="menu">
+                        <li><a href="">COV</a></li>
+                        <li><a href="">JJOO</a></li>
+                        <li><a href="">Ciclo Olímpico</a></li>
+                        <li><a href="">Disciplinas</a></li>
+                        <li><a href="">Atletas</a></li>
+                        <li><a href="">Federaciones</a></li>
+                        <li><a href="">Fotos</a></li>
+                        <li><a href="">Videos</a></li>
+                        <li><a href="">Noticias</a></li>
+                    </ul>
+                </nav>
+                <div class="logo">
+                    <img src="./img/logo.png"/>
+                </div>
+            </div> 
+            <div class="clear"></div>
+        </div>   
+        <div class="body">
+            <div class="container">
+                <div class="row">
+                    <div class="noticias-slider"></div>
+                    <div class="twitter-box"></div>
+                    <div class="actions">
+                         <div class="posiciones sq"><a href=""></a></div>
+                         <div class="olimpismo sq"><a href=""></a></div>
+                         <div class="oficiales sq"><a href=""></a></div>
+                         <div class="boletin"><a href=""></a></div>
+                         <div class="media"><a href=""></a></div>
+                         <div class="calendario"><a href=""></a></div>
+                         <div class="toronto"><a href=""></a></div>
+                         <div class="contacto"><a href=""></a></div>
+                         <div class="ubicanos"><a href=""></a></div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                 <div class="row">
+                       <div class="noticias">
+                            <h1>Noticias</h1>
+                            <p>Blah blah algun texto irá aquí sobre las noticias más importantes. bli bli
+                            blu blu. Blah blah algun texto irá aquí sobre las noticias.</p>
+                            <p class="plus"><img src='./img/plus.png'/></p>
+                     </div>
+                     <div class="atletas">
+                        <div class="toronto_atletas"><a href=""></a></div>
+                         <div class="punto_olimpico"><a href=""></a></div>
+                     </div>
+                     <div class="jug">
+                        <img src="./img/jugador.png" class="jugador"/>
+                     </div> 
                  <div class="clear"></div>
-             </div>
-              <div class="row">
-                 <div class="map">
-                    <div class="r9">
-                        <span>COV</span>
-                        <ul class="map-cov">
-                            <li><a href="">Historia del COV</a></li>
-                            <li><a href="">Autoridades</a></li>
-                            <li><a href="">El organismo</a></li>
-                            <li><a href="">Patrocinantes</a></li>
+                 </div>
+                 <div class="row">
+                     <div class="banner">Publicidad</div>
+                 </div>
+                 <div class="row">
+                     <div class="videos"></div>
+                     <div class="galeria"></div>
+                     <div class="clear"></div>
+                 </div>
+                 <div class="row">
+                     <div class="redes">
+                        <span>Redes Sociales</span>
+                        <ul class="social-bottom">
+                            <li class="twitter-b"><a href=""></a></li>
+                            <li class="facebook-b"><a href=""></a></li>
+                            <li class="instagram-b"><a href=""></a></li>
+                            <li class="youtube-b"><a href=""></a></li>
+                            <li class="linkedin-b"><a href=""></a></li>
                         </ul>
-                    </div>
-                    <div class="r9">
-                        <span>JJOO</span>
-                        <ul class="map-jjoo">
-                            <li><a href="">Participación</a></li>
-                            <li><a href="">Olímpicos Juveniles</a></li>
-                        </ul>
-                    </div>
-                    <div class="r9">
-                        <span>Ciclo Olímpico</span>
-                        <ul class="map-co">
-                            <li><a href="">Panamericanos</a></li>
-                            <li><a href="">Centroamericanos</a></li>
-                            <li><a href="">Suramericanos</a></li>
-                            <li><a href="">Puntos para JJOO</a></li>
-                        </ul>
-                    </div>
-                    <div class="r9">
-                        <span>Disciplinas</span>
-                        <ul class="disciplinas">
-                            <li><a href="">JJOO</a></li>
-                            <li><a href="">Ciclo Olímpicos</a></li>
-                        </ul>
-                    </div>
-                    <div class="r9">
-                        <span>Atletas</span>
-                        <ul class="map-atletas">
-                            <li><a href="">Registrados</a></li>
-                            <li><a href="">Participación Ciclo</a></li>
-                            <li><a href="">Participación JJOO</a></li>
-                        </ul>
-                    </div>
-                    <div class="r9">
-                        <span>Federación</span>
-                        <ul class="map-federacion">
-                            <li><a href="">Asociadas</a></li>
-                        </ul>
-                    </div>
-                    <div class="r9">
-                        <span>Fotos</span>
-                        <ul class="map-fotos">
-                            <li><a href="">Eventos deportivos</a></li>
-                        </ul>
-                    </div>
-                    <div class="r9">
-                        <span>Vídeos</span>
-                        <ul class="map-videos">
-                            <li><a href="">Eventos deportivos</a></li>
-                        </ul>
-                    </div>
-                    <div class="r9">
-                        <span>Noticias</span>
-                        <ul class="map-fotos">
-                            <li><a href="">Notas</a></li>
-                            <li><a href="">Eventos</a></li>
+                     </div>
+                     <div class="clear"></div>
+                 </div>
+                  <div class="row">
+                     <div class="map">
+                        <div class="r9">
+                            <span>COV</span>
+                            <ul class="map-cov">
+                                <li><a href="">Historia del COV</a></li>
+                                <li><a href="">Autoridades</a></li>
+                                <li><a href="">El organismo</a></li>
+                                <li><a href="">Patrocinantes</a></li>
+                            </ul>
+                        </div>
+                        <div class="r9">
+                            <span>JJOO</span>
+                            <ul class="map-jjoo">
+                                <li><a href="">Participación</a></li>
+                                <li><a href="">Olímpicos Juveniles</a></li>
+                            </ul>
+                        </div>
+                        <div class="r9">
+                            <span>Ciclo Olímpico</span>
+                            <ul class="map-co">
+                                <li><a href="">Panamericanos</a></li>
+                                <li><a href="">Centroamericanos</a></li>
+                                <li><a href="">Suramericanos</a></li>
+                                <li><a href="">Puntos para JJOO</a></li>
+                            </ul>
+                        </div>
+                        <div class="r9">
+                            <span>Disciplinas</span>
+                            <ul class="disciplinas">
+                                <li><a href="">JJOO</a></li>
+                                <li><a href="">Ciclo Olímpicos</a></li>
+                            </ul>
+                        </div>
+                        <div class="r9">
+                            <span>Atletas</span>
+                            <ul class="map-atletas">
+                                <li><a href="">Registrados</a></li>
+                                <li><a href="">Participación Ciclo</a></li>
+                                <li><a href="">Participación JJOO</a></li>
+                            </ul>
+                        </div>
+                        <div class="r9">
+                            <span>Federación</span>
+                            <ul class="map-federacion">
+                                <li><a href="">Asociadas</a></li>
+                            </ul>
+                        </div>
+                        <div class="r9">
+                            <span>Fotos</span>
+                            <ul class="map-fotos">
+                                <li><a href="">Eventos deportivos</a></li>
+                            </ul>
+                        </div>
+                        <div class="r9">
+                            <span>Vídeos</span>
+                            <ul class="map-videos">
+                                <li><a href="">Eventos deportivos</a></li>
+                            </ul>
+                        </div>
+                        <div class="r9">
+                            <span>Noticias</span>
+                            <ul class="map-fotos">
+                                <li><a href="">Notas</a></li>
+                                <li><a href="">Eventos</a></li>
+                            </ul>
+                        </div>
+                        <div class="clear"></div>
+                     </div>
+                 </div>
+                
+            </div> 
+             <div class="row last">
+                <div class="pat-ofic">Patrocinantes Oficiales</div>
+                <div class="pat-loc">
+                    <span>Patrocinantes Locales</span>
+                    <ul>
+                        <li class="cristo"><img src="./img/cristo.png" alt="Cristo"/></li>
+                        <li class="rio2016"><img src="./img/rio2016.png" alt="Rio"/></li>
+                        <li class="rio2016_2"><img src="./img/rio2016_2.png" alt="Rio"/></li>
+                        <li class="toronto2015"><img src="./img/toronto2015.png" alt="Rio"/></li>
+                        <li class="coi"><img src="./img/coi.png" alt="Rio"/></li>
+                        <li class="solidaridad"><img src="./img/rio2016.png" alt="Rio"/></li>
+                    </ul>
+                    
+                
+                </div>
+                <div class="cred">
+                    <p><img src="./img/right.png" alt="Flecha"/> Comité Olímpico Venezolano <?= date('Y')?>. Todos los derechos reservados. Desarrollador por: XXX. Imagen ADDOMO.</p>
+                    <div class="right">Siga nuestras redes sociales <img src="./img/right.png" alt="Flecha"/>
+                        <ul>
+                            <li><img src="./img/twitter_hover.png" alt="Twitter"/></li>
+                            <li><img src="./img/facebook_hover.png" alt="Facebook"/></li>
+                            <li><img src="./img/instagram_hover.png" alt="Facebook"/></li>
+                            <li><img src="./img/youtube_hover.png" alt="Facebook"/></li>
+                            <li><img src="./img/linkedin_hover.png" alt="Facebook"/></li>
                         </ul>
                     </div>
                     <div class="clear"></div>
-                 </div>
+                </div>
              </div>
-        </div> 
-    </div>   
-    <div style="height: 40px;"></div>
+            
+        </div>   
+    </div>
   </body>
 </html>
